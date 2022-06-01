10e83f0c8704390d1d6cf80d23f13e17cc7616d9
TheJeds <diegogalaxy15@gmail.com>
2022-06-01 12:03:34 -0500

    initial commit and initial style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import theJedsLogo from './images/Logo-TheJeds.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container-TheJeds-logo'>
+        <img src={theJedsLogo} className='TheJeds-logo' alt='Logo TheJeds' />
+      </div>
+      <div className='container-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
