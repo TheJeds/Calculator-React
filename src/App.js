6a533f1c1419f81d0669728b4dc7764488ee8a5c
TheJeds <diegogalaxy15@gmail.com>
2022-06-02 10:12:07 -0500

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import theJedsLogo from './images/Logo-TheJeds.png'
+import theJedsLogo from './images/Logo-TheJeds.png';
+import Button from './components/Button';
 
 function App() {
   return (
@@ -8,10 +9,34 @@ function App() {
         <img src={theJedsLogo} className='TheJeds-logo' alt='Logo TheJeds' />
       </div>
       <div className='container-calculadora'>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+
+        <div className='fila'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+          
+        <div className='fila'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+          
+        <div className='fila'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+          
         <div className='fila'></div>
       </div>
     </div>
